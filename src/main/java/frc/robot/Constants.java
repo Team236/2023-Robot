8c917244868823b0396919a5d5034102d147f1d9
matherm632 <97710496+matherm632@users.noreply.github.com>
2023-01-14 11:59:22 -0500

    more constants for testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -62,8 +62,8 @@ public final class Constants {
 }
   public static class MotorControllers {
 
-    //placeholder numbers for beginning of season because we haven't built the thing yet
-    public static final int ID_LEFT_FRONT = 10; //30
+    //placeholder numbers for beginning of season because we haven't built the thing yet, CURRENT #s = TESTBED
+    public static final int ID_LEFT_FRONT = 10; //30 - 2022 #s
     public static final int ID_RIGHT_FRONT = 15; //43
     public static final int ID_LEFT_REAR = 11; // 44
     public static final int ID_RIGHT_REAR = 16;//45
