baad4342339c96021f10b00eeb609b9dd97448bf
matherm632 <97710496+matherm632@users.noreply.github.com>
2023-01-15 10:43:28 -0500

    arm test fixes

--- src/main/java/frc/robot/commands/ArmWithAxis.java
+++ src/main/java/frc/robot/commands/ArmWithAxis.java
@@ -28,7 +28,7 @@ public class ArmWithAxis extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
-
+ 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
