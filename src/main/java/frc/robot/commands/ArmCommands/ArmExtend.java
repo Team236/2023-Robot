aa2ff02224c619036ff67e7f978c358df41c5e1d
matherm632 <97710496+matherm632@users.noreply.github.com>
2023-01-15 11:57:17 -0500

    SPEED increase

--- src/main/java/frc/robot/commands/ArmCommands/ArmExtend.java
+++ src/main/java/frc/robot/commands/ArmCommands/ArmExtend.java
@@ -32,7 +32,7 @@ public class ArmExtend extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    arm.setArmSpeed(0.5);
+    arm.setArmSpeed(0.75);
   }
 
   // Called once the command ends or is interrupted.
