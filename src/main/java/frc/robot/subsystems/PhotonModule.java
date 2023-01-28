2611336f0f3bc12a75c8a98c0dfc310e5a4c399a
team236-laptop2 <sburnham3@gmail.com>
2023-01-28 11:12:37 -0500

    temp

--- src/main/java/frc/robot/subsystems/PhotonModule.java
+++ src/main/java/frc/robot/subsystems/PhotonModule.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import java.util.List;
 
 import org.photonvision.PhotonCamera;
+// import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonTrackedTarget;
@@ -17,53 +18,57 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PhotonModule extends SubsystemBase {
   /** Creates a new PhotonModule. */
   public PhotonModule() {
     // Change this to match the name of your camera
-    PhotonCamera camera = new PhotonCamera("PhotonVision");
-    //  camera = new PhotonCamera("photonVision");
-
-    // Cam mounted facing forward, half a meter forward of center, half a meter up from center.
-    Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
-
-    // Construct PhotonPoseEstimator
-    PhotonPoseEstimator photonPoseEstimator = new PhotonPoseEstimator(AprilTagFieldLayout,
-      PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camera, robotToCam);
-
-    // Query the latest result from PhotonVision
-    var result = camera.getLatestResult();
-
-    // Check if the latest result has any targets.
-    boolean hasTargets = result.hasTargets();
-
-    // Get a list of currently tracked targets.
-    List<PhotonTrackedTarget> targets = result.getTargets();
-
-    // Get the current best target.
-    PhotonTrackedTarget target = result.getBestTarget();
-
-    // Get information from target.
-    double yaw = target.getYaw();
-    double pitch = target.getPitch();
-    double area = target.getArea();
-    double skew = target.getSkew();
-    // Transform2d pose = target.();  
-    List<TargetCorner> corners = target.getDetectedCorners();
-
-    // Get information from target.
-    int targetID = target.getFiducialId();
-    double poseAmbiguity = target.getPoseAmbiguity();
-    Transform3d bestCameraToTarget = target.getBestCameraToTarget();
-    Transform3d alternateCameraToTarget = target.getAlternateCameraToTarget();
+    PhotonCamera camera = new PhotonCamera("photonvision");
 
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // Query the latest result from PhotonVision
+    var result = camera.getLatestResult();
+
+    // Check if the latest result has any targets.
+    boolean hasTargets = result();
+
+    if (hasTargets) {
+      // Get a list of currently tracked targets.
+      List<PhotonTrackedTarget> targets = result.getTargets();
+      // Get the current best target.
+      PhotonTrackedTarget target = result.getBestTarget();
+
+      // Get information from target.
+      SmartDashboard.putNumber("TargetCount", targets.size());
+
+      // Get information from target.
+      int targetID = target.getFiducialId();
+      double poseAmbiguity = target.getPoseAmbiguity();
+      Transform3d bestCameraToTarget = target.getBestCameraToTarget();
+      Transform3d alternateCameraToTarget = target.getAlternateCameraToTarget();
+
+      double yaw = targets.getYaw();
+      SmartDashboard.putNumber("targetYaw", yaw);
+
+      double pitch = targets().getPitch();
+      SmartDashboard.putNumber("targetPitch", pitch);
+
+      double area = targets.getArea();
+      SmartDashboard.putNumber("targetArea", area);
+
+      double skew = targets.getSkew();
+      SmartDashboard.putNumber("targetSkew", skew);
+
+      Transform3d pose = targets.get();
+      SmartDashboard.putString("poseString", "x: " + pose.getX() + " y:" + pose.getY());
+
+      List<TargetCorner> corners = targets.getCorners();
+    } // end-if
 
   }
-}
+// end periodic
