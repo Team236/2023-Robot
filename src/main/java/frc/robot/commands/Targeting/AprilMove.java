037c00eed733f7f5adb03d7b8f3c959e029977b4
matherm632 <97710496+matherm632@users.noreply.github.com>
2023-02-25 08:57:20 -0500

    april tags

---
+++ src/main/java/frc/robot/commands/Targeting/AprilMove.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Targeting;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.PhotonModule;
+
+public class AprilMove extends CommandBase {
+  private Drive drive;
+  private double driveDistance = 0.5;  //meters
+  private double kPdistance, kIdistance, kDdistance; 
+  private double kPangle, kIangle, kDangle; 
+  private final PIDController distanceController, turnController;
+  private PhotonModule photonModule;
+
+  public AprilMove(Drive m_drive, PhotonModule m_photonModule) {
+    drive = m_drive; 
+    photonModule = m_photonModule;
+    distanceController = new PIDController(kPdistance, kIdistance, kDdistance);
+    turnController = new PIDController(kPangle, kIangle, kDangle);
+    addRequirements(this.drive);
+  }
+
+  @Override
+  public void initialize() {
+    distanceController.reset();
+    // goal is to maintain a set distance between the Apriltag and camera
+    distanceController.setSetpoint(driveDistance);
+    // goal is to center the robot on target
+    turnController.reset();
+    turnController.setSetpoint(0);
+
+//  dont think we need these 
+    // drive.resetLeftEncoder();
+    // drive.resetRightEncoder();
+  }
+
+  @Override
+  public void execute() {
+    double leftSpeed = distanceController.calculate(photonModule.getX()) + turnController.calculate(photonModule.getY());
+    double rightSpeed = distanceController.calculate(photonModule.getX()) - turnController.calculate(photonModule.getY());
+    SmartDashboard.putNumber("left input",-leftSpeed); // (-) For testing; Camera on back of Robot
+    SmartDashboard.putNumber("right input",-rightSpeed);
+    drive.setLeftSpeed(-leftSpeed);
+    drive.setRightSpeed(-rightSpeed);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    drive.stop();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
