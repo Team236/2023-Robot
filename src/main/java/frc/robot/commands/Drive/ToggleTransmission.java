d342f878cf476b67782d36e8f062a049fb3894d9
matherm632 <97710496+matherm632@users.noreply.github.com>
2023-03-07 19:42:41 -0500

    i forgot how toggling works :)

--- src/main/java/frc/robot/commands/Drive/ToggleTransmission.java
+++ src/main/java/frc/robot/commands/Drive/ToggleTransmission.java
@@ -42,6 +42,6 @@ public class ToggleTransmission extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return toggle;
   }
 }
