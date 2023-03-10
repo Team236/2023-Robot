6d4a3b3872641a5c558d0954fedb7b961c5a5ede
matherm632 <97710496+matherm632@users.noreply.github.com>
2023-03-10 10:38:59 -0500

    interupted condition

--- src/main/java/frc/robot/commands/Autos/DriveAtSetSpeed.java
+++ src/main/java/frc/robot/commands/Autos/DriveAtSetSpeed.java
@@ -55,7 +55,9 @@ public class DriveAtSetSpeed extends CommandBase {
   }
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drive.setBothSpeeds(0);
+  }
 
   // Returns true when the command should end.
   @Override
