6d4e969dae9e979cf36afd82a481df66111ac0f5
Mr. Burnham <sburnham3@gmail.com>
2023-03-21 22:43:39 -0400

    reset LLAngle to no errors

--- src/main/java/frc/robot/commands/Targeting/LLAngle.java
+++ src/main/java/frc/robot/commands/Targeting/LLAngle.java
@@ -5,21 +5,15 @@
 package frc.robot.commands.Targeting;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.Limelight;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import java.lang.Math;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drive;
 
 public class LLAngle extends CommandBase {
   private double kX = 0.03;  //0.005??
   private double tv, distX, errorX;
   private Drive drive;
   private double pipeline, cameraXoffset;
-  private Limelight limelight;
+ 
 
   public LLAngle(Drive _drive, double _pipeline) {
     //public LLAngle(Drive _drive, Limelight _lime, double _pipeline) {
@@ -33,25 +27,22 @@ public class LLAngle extends CommandBase {
   @Override
   public void initialize() {
     SmartDashboard.putNumber("LLangle init", pipeline);
-    //NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(0);
-    //NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(pipeline);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(0);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(pipeline);
     cameraXoffset = 0; //need to figure out
-
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3);
-    //tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
-    //distX = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
-    //errorX = distX - cameraXoffset;////
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3);
+    tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+    distX = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    errorX = distX - cameraXoffset;
 //TRY FOR errorX
-     // double errorY = NetworkTableInstance.getDefault().getTable("limelight").
-       // getIntegerTopic("targetpose_cameraspace").subscribe(new double[]{}).get()[2];  //or 0?
-    if(tv==1) {
+     double errorY = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
       if (Math.abs(errorX)>2){
-      //double x = (errorX-160)/320;
+      double x = (errorX-160)/320;
       //Establishes a minimum error in the x axis 
       SmartDashboard.putNumber("Adjust Angle, ErrorX is:", errorX);
         double steeringAdjust = kX * errorX;
@@ -62,13 +53,13 @@ public class LLAngle extends CommandBase {
       SmartDashboard.putNumber("No Shoot Target", tv);
       }
      }
-    }
+    
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     drive.stop();
-    //NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(0);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(0);
   }
 
   // Returns true when the command should end.
